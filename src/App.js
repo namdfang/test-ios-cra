c611eae6f12bacd93428f06fa3941bce1915decc
namdang <namdang06092001@gmail.com>
2024-03-09 08:52:32 +0700

    add tailwind

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Button } from 'antd';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header ">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -20,6 +20,7 @@ function App() {
           Learn React
         </a>
         <Button type='primary'>hahahahahahaha</Button>
+        <div className='h-[20px] w-[20px] bg-red-500 mt-10'></div>
       </header>
     </div>
   );
